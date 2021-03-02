9064deea1f4426f4c05f923c13e8219cc9bb10db
Camila Campos <camilacamposramos.si@gmail.com>
2021-03-01 23:06:32 -0300

    adiciona imagens para a section de pandemia

---
+++ js/carousel.js
@@ -0,0 +1,25 @@
+$('#myCarousel1').carousel({
+    interval: 1500
+})
+
+$('#myCarousel2').carousel({
+    interval: 1500
+})
+
+$('.carousel .carousel-item').each(function() {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
